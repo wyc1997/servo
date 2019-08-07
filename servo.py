c61d2d81f0913ea32e957c97fdcb3b4689bb88fb
wyc1997 <wuyangchao1997@foxmail.com>
2019-08-07 23:14:14 +0800

    finished Servo class

--- servo.py
+++ servo.py
@@ -8,4 +8,26 @@ class Servo():
         return self.cur_angle
 
     def hold(self, position):
-        
+        if position > 1 or position < 0:
+            return "invalid input: must range from 0 to 1"
+        target_angle = position * self.max_angle
+        delta_angle = target_angle - self.cur_angle
+        if delta_angle > 0:
+            if delta_angle > self.turn_rate:
+                self.cur_angle += self.turn_rate
+            else:
+                self.cur_angle += delta_angle
+        else:
+            if delta_angle < -1 * self.turn_rate:
+                self.cur_angle -= self.turn_rate
+            else:
+                self.cur_angle -= delta_angle
+
+def main():
+    s = Servo(180, 0.001)
+    while True:
+        s.hold(1)
+        print(s.getCurAngle())
+
+if __name__ == "__main__":
+    main()
