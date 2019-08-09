129ae445b568d9111e08f1db0e1b8f8ff5ad72e2
wyc1997 <wuyangchao1997@foxmail.com>
2019-08-09 17:16:24 +0800

    fixed variable sharing and able to plot on separate process

--- servo.py
+++ servo.py
@@ -1,4 +1,4 @@
-from multiprocessing import Process
+from multiprocessing import Process, Value
 from pyqtgraph.Qt import QtGui, QtCore
 import numpy as np
 import pyqtgraph as pg
@@ -7,49 +7,50 @@ class Servo():
     def __init__(self, max_angle, turn_rate):
         self.max_angle = max_angle
         self.turn_rate = turn_rate
-        self.cur_angle = 0
+        self.cur_angle = Value('d', 0)
     
     def getCurAngle(self):
-        return self.cur_angle
+        return self.cur_angle.value
 
     def hold(self, position):
         if position > 1 or position < 0:
             return "invalid input: must range from 0 to 1"
         target_angle = position * self.max_angle
-        delta_angle = target_angle - self.cur_angle
+        delta_angle = target_angle - self.cur_angle.value
         if delta_angle > 0:
             if delta_angle > self.turn_rate:
-                self.cur_angle += self.turn_rate
+                self.cur_angle.value += self.turn_rate
             else:
-                self.cur_angle += delta_angle
+                self.cur_angle.value += delta_angle
         else:
             if delta_angle < -1 * self.turn_rate:
-                self.cur_angle -= self.turn_rate
+                self.cur_angle.value -= self.turn_rate
             else:
-                self.cur_angle -= delta_angle
+                self.cur_angle.value -= delta_angle
 
-    def plotAngleAgainstTime(self):
+    def plotingUtilities(self):
         app = QtGui.QApplication([])
 
         win = pg.GraphicsWindow(title="Angle vs. Time")
         plt = win.addPlot(title="Current angle")
         curve = plt.plot(pen="y")
+        plt.setYRange(0, self.max_angle)
         data=[]
+        ptr=0
         def update():
-            data.append(self.getCurAngle())
+            # nonlocal ptr
+            # ptr+=1
+            data.append(self.cur_angle.value)
             curve.setData(data)
+            # curve.setPos(ptr, 0)
         timer = QtCore.QTimer()
         timer.timeout.connect(update)
-        timer.start(50)
+        timer.start(100)
         QtGui.QApplication.instance().exec_()
-<<<<<<< HEAD
     
     # TODO: shared variable 
-=======
-#this is a branch test
->>>>>>> test
-    def plot(self):
-        p = Process(target=self.plotAngleAgainstTime)
+    def plotAngleAgainstTime(self):
+        p = Process(target=self.plotingUtilities)
         p.start()
 
         
---
+++ test.py
@@ -0,0 +1,13 @@
+from servo import Servo
+
+def main():
+    s = Servo(180, 0.001)
+    s.plotAngleAgainstTime()
+    while True:
+        print(s.getCurAngle())
+        s.hold(1)
+
+
+if __name__ == '__main__':
+    main()
+    
